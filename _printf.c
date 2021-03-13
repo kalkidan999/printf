65f2bc118c798e58abd11e4ed5ad01cffddc5285
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-13 08:07:42 -0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,21 +5,42 @@
  */
 int _printf(const char *format, ...)
 {
-const char *p;
-int count;
-va_list argp;
-va_start(argp, format);
-if (format == NULL)
-return (-1);
-for(p=format; *p!='\0';p++)
-{
-if(*p!='%')
-{
-count += _putchar(*p);
-continue;
-}
-p++;
-}
-va_end(argp);
-return(count);
+	va_list args;
+	int chrcounter = 0, i = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+		while ((format[i] != '%') && (format[i + 1] != '\0'))
+		{
+			_putchar(format[i]);
+			i++;
+			chrcounter++;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			if (find_value(format[i]) == NULL)
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				chrcounter = chrcounter + 2;
+			}
+			else
+			{
+				chrcounter += (find_value(format[i]))(args);
+			}
+			i++;
+		}
+		if (format[i] != '\0' && format[i] != '%')
+		{
+			_putchar(format[i]);
+			chrcounter++;
+			i++;
+		}
+	}
+	return (chrcounter);
 }
