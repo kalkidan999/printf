6f201ba10a3e9d9a90e5aaca9075b10d67441063
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-13 08:15:24 -0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,42 +5,29 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int chrcounter = 0, i = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	while (format[i] != '\0')
-	{
-		while ((format[i] != '%') && (format[i + 1] != '\0'))
-		{
-			_putchar(format[i]);
-			i++;
-			chrcounter++;
-		}
-		if (format[i] == '%')
-		{
-			i++;
-			if (find_value(format[i]) = NULL)
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				chrcounter = chrcounter + 2;
-			}
-			else
-			{
-				chrcounter += (find_value(format[i]))(args);
-			}
-			i++;
-		}
-		if (format[i] != '\0' && format[i] != '%')
-		{
-			_putchar(format[i]);
-			chrcounter++;
-			i++;
-		}
-	}
-	return (chrcounter);
+const char *p;
+int i;
+unsigned u;
+char *s;
+int count =0;
+va_list argp;
+va_start(argp, format);
+p=frmt;
+for(p=format; *p!='\0';p++)
+{
+if(*p!='%')
+{
+count += putchar(*p);
+	continue;
+}
+p++;
+switch(*p)
+{
+case '%': 
+count += putchar('%');
+break;
+}
+}
+va_end(argp);
+return(count);
 }
