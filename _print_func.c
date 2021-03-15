da70d09064d51738553f6d9e6b4ece7a46462eb8
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-15 16:02:13 +0300

    Update _print_func.c

--- _print_func.c
+++ _print_func.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * print_c - print char
  * @list: va_list
  * Return: string
  */
-int print_c(va_list arg)
+int *print_c(va_list arg)
 {
 	int i;
 
 	i = va_arg(arg, int);
-	_putchar(i);
+	putchar(i);
 	return (1);
 }
 /**
