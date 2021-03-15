0af77bd4ed307d3c5f4a9e93bba9a5e9df228338
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-15 15:48:39 +0300

    Update _print_func.c

--- _print_func.c
+++ _print_func.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "holberton.h"
-#include <unistd.h>
+
 /**
  * print_c - print char
  * @list: va_list
@@ -9,25 +9,13 @@
  */
 char *print_c(va_list list)
 {
+	static char s[2] = {0, '\0'};
 
-	int i;
-
-	i = va_arg(list, int);
-	_putchar(i);
-	return (1);
+	s[0] = va_arg(list, int);
+	if (s[0] == '\0')
+		return (" ");
+	return (s);
 }
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 /**
  * print_s - print string
  * @list: va_list
