fe199796a0e6c1de2be32904023c8f191426eaac
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-03-16 08:54:03 +0300

    Update print_i.c

--- print_i.c
+++ print_i.c
@@ -1,71 +1,84 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "holberton.h"
-#include <stdlib.h>
+
 /**
- * _abs - absolute value
- * @n: int
- * Return: n
+ * print_int - prints an integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-int _abs(int n)
+int print_int(va_list l, flags_t *f)
 {
-	if (n < 0)
-		n *= -1;
-	return (n);
+	int n = va_arg(l, int);
+	int res = count_digit(n);
+
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+	if (n <= 0)
+		res++;
+	print_number(n);
+	return (res);
 }
+
 /**
- * print_i - print integer
- * @list: va_list
- * Return: string
+ * print_unsigned - prints an unsigned integer
+ * @l: va_list of arguments from _printf
+ * @f: pointer to the struct flags determining
+ * if a flag is passed to _printf
+ * Return: number of char printed
  */
-char *print_i(va_list list)
+int print_unsigned(va_list l, flags_t *f)
 {
-	int digit = 0, i = 0, neg = 0;
-	int divisor = 1000000000;
-	char *output;
-	int num = va_arg(list, int);
-	int len = _numlen(num);
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
-	output = malloc(len * sizeof(char) + 1 + neg);
+	(void)f;
+	return (_puts(str));
+}
 
-	if (output == NULL)
+/**
+ * print_number - helper function that loops through
+ * an integer and prints all its digits
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
 	{
-		free(output);
-		return (0);
+		_putchar('-');
+		n1 = -n;
 	}
+	else
+		n1 = n;
 
-	if (num < 0)
-	{
-		neg++;
-	}
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar((n1 % 10) + '0');
+}
 
-	if (num < 0)
-	{
-		output[i] = '-';
-		i++;
-	}
+/**
+ * count_digit - returns the number of digits in an integer
+ * for _printf
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
 
-	if (num > -10 && num < 10)
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
 	{
-		output[i] = _abs(num) + '0';
-		return (output);
+		u /= 10;
+		d++;
 	}
-
-	while (divisor)
-	{
-		digit = (num / divisor) % 10;
-		if (num < 0)
-		{
-			digit = _abs(digit);
-		}
-		if (digit != 0 || (len >= 0  && output[i - 1] >= '0'))
-		{
-			output[i] = digit + '0';
-			i++;
-			len--;
-		}
-		divisor /= 10;
-	}
-	output[i] = '\0';
-	return (output);
+	return (d);
 }
